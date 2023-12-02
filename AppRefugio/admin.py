bd0fe7cb515632d8b8c2cde1dda7c8904bd5c249
Javier Barbera <javier_barbera88@hotmail.com>
2023-12-02 15:57:00 -0300

    Commit 4

--- AppRefugio/admin.py
+++ AppRefugio/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from AppRefugio import models
 
-# Register your models here.
+
+admin.site.register(models.Libro)
+admin.site.register(models.Disco)
+admin.site.register(models.Pelicula)
--- AppRefugio/models.py
+++ AppRefugio/models.py
@@ -7,11 +7,21 @@ class Libro(models.Model):
     autor= models.CharField(max_length=50)
     sinopsis= models.TextField()
 
+    def __str__(self):
+        return f"Título: {self.titulo} - Autor: {self.autor} - Sinopsis: {self.sinopsis}"
+        
+
 class Disco(models.Model):
     titulo= models.CharField(max_length=50)
     interprete= models.CharField(max_length=50)
  
+    def __str__(self):
+        return f"Título: {self.titulo} - Interprete: {self.interprete}"
+
 class Pelicula(models.Model):
     titulo= models.CharField(max_length=50)
     director= models.CharField(max_length=50)
     sinopsis= models.TextField()
+    
+    def __str__(self):
+        return f"Título: {self.titulo} - Director: {self.director} - Sinopsis: {self.sinopsis}"
